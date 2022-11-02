5c45a41f55322925a198001afcf292d3bdafd576
Zhao KK <kikakkz@hotmail.com>
2022-11-02 15:32:34 +0000

    Add app coin

---
+++ pkg/client/coin/appcoin/client.go
@@ -0,0 +1,199 @@
+//nolint:dupl
+package appcoin
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	npool "github.com/NpoolPlatform/message/npool/chain/mgr/v1/coin/appcoin"
+
+	constant "github.com/NpoolPlatform/chain-manager/pkg/message/const"
+)
+
+var timeout = 10 * time.Second
+
+type handler func(context.Context, npool.ManagerClient) (cruder.Any, error)
+
+func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	conn, err := grpc2.GetGRPCConn(constant.ServiceName, grpc2.GRPCTAG)
+	if err != nil {
+		return nil, fmt.Errorf("fail get appcoin connection: %v", err)
+	}
+
+	defer conn.Close()
+
+	cli := npool.NewManagerClient(conn)
+
+	return handler(_ctx, cli)
+}
+
+func CreateAppCoin(ctx context.Context, in *npool.AppCoinReq) (*npool.AppCoin, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.CreateAppCoin(ctx, &npool.CreateAppCoinRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail create appcoin: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fail create appcoin: %v", err)
+	}
+	return info.(*npool.AppCoin), nil
+}
+
+func CreateAppCoins(ctx context.Context, in []*npool.AppCoinReq) ([]*npool.AppCoin, error) {
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.CreateAppCoins(ctx, &npool.CreateAppCoinsRequest{
+			Infos: in,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail create appcoins: %v", err)
+		}
+		return resp.GetInfos(), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fail create appcoins: %v", err)
+	}
+	return infos.([]*npool.AppCoin), nil
+}
+
+func UpdateAppCoin(ctx context.Context, in *npool.AppCoinReq) (*npool.AppCoin, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.UpdateAppCoin(ctx, &npool.UpdateAppCoinRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail add appcoin: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fail update appcoin: %v", err)
+	}
+	return info.(*npool.AppCoin), nil
+}
+
+func GetAppCoin(ctx context.Context, id string) (*npool.AppCoin, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.GetAppCoin(ctx, &npool.GetAppCoinRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail get appcoin: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fail get appcoin: %v", err)
+	}
+	return info.(*npool.AppCoin), nil
+}
+
+func GetAppCoinOnly(ctx context.Context, conds *npool.Conds) (*npool.AppCoin, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.GetAppCoinOnly(ctx, &npool.GetAppCoinOnlyRequest{
+			Conds: conds,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail get appcoin: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fail get appcoin: %v", err)
+	}
+	return info.(*npool.AppCoin), nil
+}
+
+func GetAppCoins(ctx context.Context, conds *npool.Conds, limit, offset int32) ([]*npool.AppCoin, uint32, error) {
+	var total uint32
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.GetAppCoins(ctx, &npool.GetAppCoinsRequest{
+			Conds:  conds,
+			Limit:  limit,
+			Offset: offset,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail get appcoins: %v", err)
+		}
+		total = resp.GetTotal()
+		return resp.GetInfos(), nil
+	})
+	if err != nil {
+		return nil, 0, fmt.Errorf("fail get appcoins: %v", err)
+	}
+	return infos.([]*npool.AppCoin), total, nil
+}
+
+func ExistAppCoin(ctx context.Context, id string) (bool, error) {
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.ExistAppCoin(ctx, &npool.ExistAppCoinRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail get appcoin: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return false, fmt.Errorf("fail get appcoin: %v", err)
+	}
+	return infos.(bool), nil
+}
+
+func ExistAppCoinConds(ctx context.Context, conds *npool.Conds) (bool, error) {
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.ExistAppCoinConds(ctx, &npool.ExistAppCoinCondsRequest{
+			Conds: conds,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail get appcoin: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return false, fmt.Errorf("fail get appcoin: %v", err)
+	}
+	return infos.(bool), nil
+}
+
+func CountAppCoins(ctx context.Context, conds *npool.Conds) (uint32, error) {
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.CountAppCoins(ctx, &npool.CountAppCoinsRequest{
+			Conds: conds,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail count appcoin: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return 0, fmt.Errorf("fail count appcoin: %v", err)
+	}
+	return infos.(uint32), nil
+}
+
+func DeleteAppCoin(ctx context.Context, id string) (*npool.AppCoin, error) {
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.DeleteAppCoin(ctx, &npool.DeleteAppCoinRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail delete appcoin: %v", err)
+		}
+		return resp.GetInfo(), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fail delete appcoin: %v", err)
+	}
+	return infos.(*npool.AppCoin), nil
+}
---
+++ pkg/client/coin/appcoin/client_test.go
@@ -0,0 +1,119 @@
+package appcoin
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/config"
+
+	"bou.ke/monkey"
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/NpoolPlatform/chain-manager/pkg/testinit"
+
+	npool "github.com/NpoolPlatform/message/npool/chain/mgr/v1/coin/appcoin"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/google/uuid"
+)
+
+func init() {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
+	}
+}
+
+var appCoin = &npool.AppCoin{
+	ID:                       uuid.NewString(),
+	AppID:                    uuid.NewString(),
+	CoinTypeID:               uuid.NewString(),
+	Name:                     uuid.NewString(),
+	Logo:                     uuid.NewString(),
+	ForPay:                   false,
+	WithdrawAutoReviewAmount: "89.123",
+}
+
+var appCoinReq = &npool.AppCoinReq{
+	ID:                       &appCoin.ID,
+	AppID:                    &appCoin.AppID,
+	CoinTypeID:               &appCoin.CoinTypeID,
+	Name:                     &appCoin.Name,
+	Logo:                     &appCoin.Logo,
+	ForPay:                   &appCoin.ForPay,
+	WithdrawAutoReviewAmount: &appCoin.WithdrawAutoReviewAmount,
+}
+
+func createAppCoin(t *testing.T) {
+	info, err := CreateAppCoin(context.Background(), appCoinReq)
+	if assert.Nil(t, err) {
+		appCoin.CreatedAt = info.CreatedAt
+		appCoin.UpdatedAt = info.UpdatedAt
+		assert.Equal(t, appCoin, info)
+	}
+}
+
+func updateAppCoin(t *testing.T) {
+	name := uuid.NewString()
+	withdrawAutoReviewdAmount := "81.0234"
+	forPay := true
+
+	appCoinReq.Name = &name
+	appCoinReq.WithdrawAutoReviewAmount = &withdrawAutoReviewdAmount
+	appCoinReq.ForPay = &forPay
+
+	appCoin.Name = name
+	appCoin.WithdrawAutoReviewAmount = withdrawAutoReviewdAmount
+	appCoin.ForPay = forPay
+
+	info, err := UpdateAppCoin(context.Background(), appCoinReq)
+	if assert.Nil(t, err) {
+		appCoin.UpdatedAt = info.UpdatedAt
+		assert.Equal(t, appCoin, info)
+	}
+}
+
+func createAppCoins(t *testing.T) {
+}
+
+func getAppCoin(t *testing.T) {
+	info, err := GetAppCoin(context.Background(), appCoin.ID)
+	if assert.Nil(t, err) {
+		assert.Equal(t, appCoin, info)
+	}
+}
+
+func deleteAppCoin(t *testing.T) {
+	info, err := DeleteAppCoin(context.Background(), appCoin.ID)
+	if assert.Nil(t, err) {
+		assert.Equal(t, appCoin, info)
+	}
+
+	_, err = GetAppCoin(context.Background(), info.ID)
+	assert.NotNil(t, err)
+}
+
+func TestClient(t *testing.T) {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+
+	gport := config.GetIntValueWithNameSpace("", config.KeyGRPCPort)
+
+	monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
+		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	})
+
+	t.Run("createAppCoin", createAppCoin)
+	t.Run("updateAppCoin", updateAppCoin)
+	t.Run("createAppCoins", createAppCoins)
+	t.Run("getAppCoin", getAppCoin)
+	t.Run("deleteAppCoin", deleteAppCoin)
+}
